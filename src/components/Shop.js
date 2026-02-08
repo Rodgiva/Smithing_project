d9a5aa71e913a476de7c1b11c2ca725be7b61628
Avigdor <avigdorpartouche@gmail.com>
2026-02-08 18:59:18 +0200

    .

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -7,6 +7,15 @@ import mersh4 from "../assets/mershandises/4.jpg";
 import mersh5 from "../assets/mershandises/5.jpg";
 import mersh6 from "../assets/mershandises/6.jpg";
 
+const items = [
+  { img: mersh1, name: "Bowie", price: "350 ₪" },
+  { img: mersh2, name: "Chasse", price: "450 ₪" },
+  { img: mersh3, name: "Raté", price: "200 ₪" },
+  { img: mersh4, name: "Olivier", price: "250 ₪" },
+  { img: mersh5, name: "Projet sabre", price: "450 ₪" },
+  { img: mersh6, name: "Dernier projet", price: "500 ₪" },
+];
+
 const Shop = (props) => {
   const [isVisible, setIsVisible] = useState(false);
   const handleEnter = () => {
@@ -21,36 +30,13 @@ const Shop = (props) => {
         <section className={"section"}>
           <div className={`${isVisible ? "fade-in" : "fade-out"}`}>
             <div className="mershandises">
-              <div className="mershandise">
-                <img src={mersh1} className="image-mersh" />
-                <h1>Bowie</h1>
-                <h1>350 ₪</h1>
-              </div>
-              <div className="mershandise">
-                <img src={mersh2} className="image-mersh" />
-                <h1>Chasse</h1>
-                <h1>450 ₪</h1>
-              </div>
-              <div className="mershandise">
-                <img src={mersh3} className="image-mersh" />
-                <h1>Raté</h1>
-                <h1>200 ₪</h1>
-              </div>
-              <div className="mershandise">
-                <img src={mersh4} className="image-mersh" />
-                <h1>Olivier</h1>
-                <h1>250 ₪</h1>
-              </div>
-              <div className="mershandise">
-                <img src={mersh5} className="image-mersh" />
-                <h1>Projet sabre</h1>
-                <h1>450 ₪</h1>
-              </div>
-              <div className="mershandise">
-                <img src={mersh6} className="image-mersh" />
-                <h1>Dernier projet</h1>
-                <h1>500 ₪</h1>
-              </div>
+              {items.map((item, i) => (
+                <div className="mershandise" key={i}>
+                  <img src={item.img} className="image-mersh" alt={item.name} />
+                  <h1>{item.name}</h1>
+                  <h1>{item.price}</h1>
+                </div>
+              ))}
             </div>
           </div>
         </section>
